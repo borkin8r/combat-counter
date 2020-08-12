6bb481372fa5737d983cece8ae68d0a536422fed
John Bork <jlbork@mtu.edu>
2020-08-11 23:25:13 -0500

    setup html

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,34 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+  <div class="combat-counter">
+    <h1>Combat Counter</h1>
+  <div>
+    <label>Count: {{ count }}</label>
+    <button>+</button>
+    <button>-</button>
+  </div>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+      <li>
+        <label>Name: 
+          <input type="text">
+        </label>
+        <label>Action: 
+          <input type="text">
+        </label>
+        <label>Seconds Until Action: 
+          <input type="text">
+        </label>
+        <button>X</button>
+      </li>
     </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <button>Add Combatant</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Combat Counter',
   props: {
-    msg: String
+    count: Number
   }
 }
 </script>
@@ -46,11 +40,13 @@ h3 {
 }
 ul {
   list-style-type: none;
-  padding: 0;
+  padding: 0.5rem;
+  border: solid 1px;
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
+  display: inline;
+  border: solid 1px;
+  padding: 0.5rem;
 }
 a {
   color: #42b983;
